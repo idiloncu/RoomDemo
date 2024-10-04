5f8dd15750c2a16d005ea253900d3eff50a55195
octopus-idil <oncuidil@gmail.com>
2024-10-04 14:58:52 +0300

    roomdb applied

---
+++ app/src/main/java/com/example/roomdemo/EmployeeEntity.kt
@@ -0,0 +1,15 @@
+package com.example.roomdemo
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Ignore
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "employee-table")
+data class EmployeeEntity(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0,
+    var name: String = "",
+    @ColumnInfo(name = "email-id")
+    var email: String = ""
+)
